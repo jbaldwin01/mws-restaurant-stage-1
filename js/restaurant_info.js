1ed4daf9fc7b52c38eab3a74286a5a3956cffa19
Jeffrey Baldwin <jbaldwin01@snet.net>
2018-12-17 11:54:43 -0500

    Make responsive and accessible

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -87,9 +87,10 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   address.innerHTML = restaurant.address;
 
   const image = document.getElementById('restaurant-img');
-  image.className = 'restaurant-img'
+  image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-
+  image.alt = 'Restaurant Image';
+  
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
 
